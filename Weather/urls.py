24817175c5085195c1647da5ba3c08b36c0b2c6b
Owais Shaikh <66899497+zoot-nix@users.noreply.github.com>
2021-10-04 16:10:43 +0530

    Update urls.py

--- Weather/urls.py
+++ Weather/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.urls.conf import include
 from django.views.static import serve
 from django.conf.urls import url
-
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
